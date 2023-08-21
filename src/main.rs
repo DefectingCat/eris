c1f6d1ddc84ebfabba8b23303786a49ef4d3b093
DefectingCat <i@defect.ink>
2023-08-21 16:23:20 +0800

    add zip create

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use std::{
+    fs::{self, File},
+    io,
+    path::Path,
+};
+use zip::ZipArchive;
+
+fn main() -> Result<()> {
+    // let file_name = Path::from("./test.zip");
+    let file = File::open("./test/test.zip")?;
+
+    let mut archive = ZipArchive::new(file)?;
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        let outpath = match file.enclosed_name() {
+            Some(path) => path.to_owned(),
+            None => continue,
+        };
+
+        if file.name().ends_with('/') {
+            println!("File {} extracted to \"{}\"", i, outpath.display());
+            fs::create_dir_all(&outpath)?;
+        } else {
+            println!(
+                "File {} extracted to \"{}\" ({} bytes)",
+                i,
+                outpath.display(),
+                file.size()
+            );
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    fs::create_dir_all(p)?;
+                }
+            }
+            let mut outfile = fs::File::create(&outpath)?;
+            io::copy(&mut file, &mut outfile)?;
+        }
+    }
+
+    Ok(())
 }
