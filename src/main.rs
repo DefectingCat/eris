6fee3d053dc1127681db9d0b59a59bd0c4c6e8c3
DefectingCat <i@defect.ink>
2023-08-21 17:36:32 +0800

    add html parse
    rewrite body tag into html file

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,49 @@
 use anyhow::Result;
-use std::path::PathBuf;
+use scraper::{Html, Selector};
+use std::{
+    fs::{self, File},
+    io::{Read, Write},
+    path::PathBuf,
+};
 
 use ziper::Ziper;
 
+mod gegenees;
 mod ziper;
 
 fn main() -> Result<()> {
     let path = PathBuf::from("./test/test.zip");
     let mut ziper = Ziper::new(&path)?;
     ziper.unzip(Some("./test"))?;
+
+    let prefix = PathBuf::from("./test");
+    let index = {
+        let mut p = prefix.clone();
+        p.push("index.html");
+        p
+    };
+    let mut index_file = File::options()
+        .read(true)
+        .write(true)
+        .append(true)
+        .open(&index)?;
+    let mut index = String::new();
+    index_file.read_to_string(&mut index)?;
+    let doc = Html::parse_document(&index);
+    let body_selector = Selector::parse("body").unwrap();
+    // body tag should has one
+    let body = doc.select(&body_selector).next().unwrap();
+    index_file.set_len(0)?;
+    // rewrite body tag to html file
+    index_file.write_all(body.html().as_bytes())?;
+
+    let style = {
+        let mut p = prefix;
+        p.push("style.css");
+        p
+    };
+    let _style_file = fs::read_to_string(style)?;
+    let _style = Html::new_fragment();
 
     Ok(())
 }
