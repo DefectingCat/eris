58c59e8d4e22c2316b899096db2a5f76161422ec
DefectingCat <i@defect.ink>
2023-08-21 16:52:04 +0800

    struct zip to ziper module

--- src/main.rs
+++ src/main.rs
@@ -5,43 +5,14 @@ use std::{
     path::{Path, PathBuf},
 };
 use zip::ZipArchive;
+use ziper::Ziper;
+
+mod ziper;
 
 fn main() -> Result<()> {
-    // let file_name = Path::from("./test.zip");
-    let file = File::open("./test/test.zip")?;
-
-    let mut archive = ZipArchive::new(file)?;
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-        let outpath = match file.enclosed_name() {
-            Some(path) => {
-                let mut p = PathBuf::from("./test");
-                p.push(path);
-                p
-            }
-            None => continue,
-        };
-
-        if file.name().ends_with('/') {
-            println!("File {} extracted to \"{}\"", i, outpath.display());
-            fs::create_dir_all(&outpath)?;
-        } else {
-            println!(
-                "File {} extracted to \"{}\" ({} bytes)",
-                i,
-                outpath.display(),
-                file.size()
-            );
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    fs::create_dir_all(p)?;
-                }
-            }
-            let mut outfile = fs::File::create(&outpath)?;
-            io::copy(&mut file, &mut outfile)?;
-        }
-    }
+    let path = PathBuf::from("./test/test.zip");
+    let mut ziper = Ziper::new(&path)?;
+    ziper.unzip()?;
 
     Ok(())
 }
---
+++ src/ziper.rs
@@ -0,0 +1,56 @@
+use std::{
+    fs::{self, File},
+    io,
+    path::{Path, PathBuf},
+};
+
+use anyhow::Result;
+use zip::ZipArchive;
+
+pub struct Ziper {
+    archive: ZipArchive<File>,
+}
+
+impl Ziper {
+    pub fn new(path: &Path) -> Result<Self> {
+        let file = File::open(path)?;
+
+        Ok(Self {
+            archive: ZipArchive::new(file)?,
+        })
+    }
+
+    pub fn unzip(&mut self) -> Result<()> {
+        for i in 0..self.archive.len() {
+            let mut file = self.archive.by_index(i)?;
+            let outpath = match file.enclosed_name() {
+                Some(path) => {
+                    let mut p = PathBuf::from("./test");
+                    p.push(path);
+                    p
+                }
+                None => continue,
+            };
+
+            if file.name().ends_with('/') {
+                println!("File {} extracted to \"{}\"", i, outpath.display());
+                fs::create_dir_all(&outpath)?;
+            } else {
+                println!(
+                    "File {} extracted to \"{}\" ({} bytes)",
+                    i,
+                    outpath.display(),
+                    file.size()
+                );
+                if let Some(p) = outpath.parent() {
+                    if !p.exists() {
+                        fs::create_dir_all(p)?;
+                    }
+                }
+                let mut outfile = fs::File::create(&outpath)?;
+                io::copy(&mut file, &mut outfile)?;
+            }
+        }
+        Ok(())
+    }
+}
