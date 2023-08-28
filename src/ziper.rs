886fc04d4832365ff403b8658347d509d25714e7
DefectingCat <i@defect.ink>
2023-08-28 14:44:25 +0800

    add directory walker

--- src/ziper.rs
+++ src/ziper.rs
@@ -1,11 +1,11 @@
 use std::{
-    fs::{self, File},
-    io,
+    fs::{self, DirEntry, File},
+    io::{self, Read, Seek, Write},
     path::{Path, PathBuf},
 };
 
 use anyhow::Result;
-use zip::ZipArchive;
+use zip::{write::FileOptions, ZipArchive};
 
 pub struct Ziper {
     archive: ZipArchive<File>,
@@ -18,6 +18,48 @@ impl Ziper {
         Ok(Self {
             archive: ZipArchive::new(file)?,
         })
+    }
+
+    pub fn zip_dir<T>(
+        it: &mut dyn Iterator<Item = DirEntry>,
+        prefix: &str,
+        writer: T,
+        method: zip::CompressionMethod,
+    ) -> Result<()>
+    where
+        T: Write + Seek,
+    {
+        let mut zip = zip::ZipWriter::new(writer);
+        let options = FileOptions::default()
+            .compression_method(method)
+            .unix_permissions(0o755);
+
+        let mut buffer = Vec::new();
+        for entry in it {
+            let path = entry.path();
+            let name = path.strip_prefix(Path::new(prefix)).unwrap();
+
+            // Write file or directory explicitly
+            // Some unzip tools unzip files with directory paths correctly, some do not!
+            if path.is_file() {
+                println!("Adding file {path:?} as {name:?} ...");
+                #[allow(deprecated)]
+                zip.start_file_from_path(name, options)?;
+                let mut f = File::open(path)?;
+
+                f.read_to_end(&mut buffer)?;
+                zip.write_all(&buffer)?;
+                buffer.clear();
+            } else if !name.as_os_str().is_empty() {
+                // Only if not root! Avoids path spec / warning
+                // and mapname conversion failed error on unzip
+                println!("Adding dir {path:?} as {name:?} ...");
+                #[allow(deprecated)]
+                zip.add_directory_from_path(name, options)?;
+            }
+        }
+        zip.finish()?;
+        Ok(())
     }
 
     pub fn unzip(&mut self, prefix: Option<&str>) -> Result<()> {
